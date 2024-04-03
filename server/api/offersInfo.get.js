208305903c50d02c0c594c3d77e0181327046690
AandLSistemSecurity <stanislav.alexandru@gmail.com>
2024-04-03 16:36:08 +0300

    Fetch offers test 5

--- server/api/offersInfo.get.js
+++ server/api/offersInfo.get.js
@@ -1,7 +1,7 @@
 import connection from '../modules/postgres.js'
 export default defineEventHandler(async (event) => {
     try {
-        const data = connection.query('SELECT * FROM lt_offers')
+        const data = await connection.query('SELECT * FROM lt_offers')
         console.log(data)
         return data
     } catch (error) {
